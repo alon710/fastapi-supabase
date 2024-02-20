ad61f5b4fdc8b7e05852f133bc866ea98666c035
Alon Barad <Alon710@gmail.com>
2024-02-20 14:15:06 +0200

    Finished user handling API

--- app/server.py
+++ app/server.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from app.database.dal import DataAccessLayer
+from app.database.db import Database
+from app.routes import user
+
 load_dotenv()
 
 app = FastAPI(
@@ -15,12 +19,23 @@ app = FastAPI(
 )
 
 
+def get_connection():
+    return DataAccessLayer(db=Database)
+
+
+@app.on_event("startup")
+async def startup():
+    Database.Base.metadata.create_all(bind=Database.engine)
+
+
 @app.get("/", include_in_schema=False)
 async def index():
     return RedirectResponse(url="/docs")
 
 
 if __name__ == "__main__":
+    app.include_router(user.router)
+
     uvicorn.run(
         app,
         port=8000 if os.getenv("ENV") == "DEBUG" else 80,
